7e88a7e9a483fa9dbb20c817e9e270053f9a9c6b
Dmitry913 <st077178@student.spbu.ru>
2021-08-18 20:33:55 +0300

    create tempate

---
+++ src/test/java/code/sharing/demo/CodeSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package code.sharing.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeSharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
