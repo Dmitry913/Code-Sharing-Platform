16fb49467b9d3ede78c075e59e4b0be71f127c75
Dmitry913 <st077178@student.spbu.ru>
2021-08-20 17:34:30 +0300

    adding Post methods

--- src/main/java/code/sharing/demo/models/Code.java
+++ src/main/java/code/sharing/demo/models/Code.java
@@ -4,12 +4,18 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
+
+
 @Component
 public class Code {
     private String code = """
             public static void main(String[] args) {
                     SpringApplication.run(CodeSharingPlatform.class, args);
                 }""";
+
+    private LocalDateTime date = LocalDateTime.now().withNano(0);
 
     public String returnHtmlCode() {
         String result = """
@@ -18,10 +24,28 @@ public class Code {
                     <title>Code</title>
                 </head>
                 <body>
-                    <pre>%s</pre>
+                    <span id="load_date">%s</span>
+                    <pre id="code_snippet">%s</pre>
                 </body>
                 </html>""";
-        return String.format(result, code);
+        return String.format(result, date.toString(), code);
+    }
+
+    public String returnHtmlForInput() {
+        return """
+                <html>
+                <head>
+                    <title>Create</title>
+                </head>
+                <body>
+                    <script src="static/converter.js"></script>
+                    <textarea id="code_snippet"></textarea>
+                    <button id="send_snippet" type="submit" onclick="send()">Submit</button>
+                </body>
+                </html>
+                
+                
+                """;
     }
 
     public Code returnJsonCode() {
@@ -30,6 +54,15 @@ public class Code {
 
     public String getCode() {
         return code;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+        date = LocalDateTime.now().withNano(0);
     }
 
 }
--- src/main/java/code/sharing/demo/presentationLayer/CodeController.java
+++ src/main/java/code/sharing/demo/presentationLayer/CodeController.java
@@ -1,20 +1,14 @@
 package code.sharing.demo.presentationLayer;
 
 
-
 import code.sharing.demo.models.Code;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonObjectFormatVisitor;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 
 
 @RestController
@@ -32,16 +26,29 @@ public class CodeController {
 
     @GetMapping("/api/code")
     public ResponseEntity<Code> getJsonCode() {
-        System.out.println("\n\n\nAPI CODE");
+//        System.out.println("\n\n\nAPI CODE");
         responseHeaders.set("Content-Type", "application/json");
         return ResponseEntity.ok().headers(responseHeaders).body(code.returnJsonCode());
     }
+
     @GetMapping("/code")
     public ResponseEntity<String> getHtmlCode() {
-        System.out.println("\n\n\nCODE");
+//        System.out.println("\n\n\nCODE");
         responseHeaders.set("Content-Type", "text/html");
         return ResponseEntity.ok().headers(responseHeaders).body(code.returnHtmlCode());
     }
 
+    @PostMapping("/api/code/new")
+    public ResponseEntity<String> postApiCode(@RequestBody Code code) {
+        this.code.setCode(code.getCode());
+        responseHeaders.set("Content-Type", "application/json");
+        return ResponseEntity.ok().headers(responseHeaders).body("{}");
+    }
+
+    @GetMapping("/code/new")
+    public ResponseEntity<String> postHtmlCode() {
+        responseHeaders.set("Content-Type", "text/html");
+        return ResponseEntity.ok().headers(responseHeaders).body(code.returnHtmlForInput());
+    }
 }
 
